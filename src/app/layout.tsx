688bea96441793a101ac4e18583fdef3f35445e6
w0dm4n <w0dm4n@hotmail.fr>
2025-05-02 05:31:08 +0200

    favicon updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,13 +17,34 @@ export const metadata: Metadata = {
     canonical: '/',
   },
   icons: {
-    icon: [
-      { url: '/images/Helios-Testnet.png' },
-      { url: '/images/Helios-Testnet.png', sizes: '32x32' },
-      { url: '/images/Helios-Testnet.png', sizes: '16x16' }
-    ],
-    apple: [
-      { url: '/images/Helios-Testnet.png' }
+    icon: '/favicon/favicon.ico',
+    shortcut: '/favicon/favicon.ico',
+    apple: '/favicon/apple-touch-icon.png',
+    other: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/favicon/favicon-16x16.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/favicon/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        url: '/favicon/android-chrome-192x192.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '512x512',
+        url: '/favicon/android-chrome-512x512.png',
+      }
     ]
   },
   openGraph: {
